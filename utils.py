69f6bcc7afab0024074e850830dc95cf5b1d05ca
adhityaputra1 <151349672+adhityaputra1@users.noreply.github.com>
2025-07-20 17:28:16 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,29 @@
-import pytesseract
+import requests
 from PIL import Image
 import pandas as pd
 
 def extract_text(image_file):
-    image = Image.open(image_file)
-    text = pytesseract.image_to_string(image, lang='ind')
-    return text
+    api_key = "K85885739088957"  # Pakai API key kamu
+    payload = {
+        'isOverlayRequired': False,
+        'apikey': api_key,
+        'language': 'ind'
+    }
+
+    files = {
+        'file': (image_file.name, image_file.getvalue())
+    }
+
+    response = requests.post('https://api.ocr.space/parse/image',
+                             data=payload,
+                             files=files)
+    result = response.json()
+
+    try:
+        parsed_text = result['ParsedResults'][0]['ParsedText']
+        return parsed_text
+    except Exception as e:
+        return f"OCR failed: {e}"
 
 def save_to_excel(data_dict, filename='hasil_ekstraksi.xlsx'):
     df = pd.DataFrame([data_dict])
